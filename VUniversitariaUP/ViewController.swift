147ae0e13259812398ec5ce1ca6d2e57050ac5ce
visitaupb <visitaupb@imac.local>
2017-06-01 20:11:30 -0500

    Initial Commit

---
+++ VUniversitariaUP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VUniversitariaUP
+//
+//  Created by visitaupb on 01/06/17.
+//  Copyright © 2017 visitaupb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
